c1dfcf706183d32e2c0ee79ef8bbac4958b3ed80
BlazeEMP <blazelitanui@gmail.com>
2025-01-04 21:51:10 -0600

    short list added

--- vite-React-Portfolio/src/pages/Resume.jsx
+++ vite-React-Portfolio/src/pages/Resume.jsx
@@ -10,5 +10,11 @@ export default function Resume() {
             <div className='resume-link-container'>
                 <a href="https://docs.google.com/document/d/1lVXtTj2VvZO5hBZav0dI20Gu58boaEBrBxorxWOQVfU/edit?usp=sharing" target="_blank" rel="noreferrer" className='resume-link'>Download Resume</a>
             </div>
+            <ul>
+                <span>Developer experience and skills in...</span>
+                <li>Front end HTML, CSS, Bootstrap, JS</li>
+                <li>Framework developer flow using React and Vite build tools</li>
+                <li>Back end Postgres database usage</li>
+            </ul>
         </div>)
 }
